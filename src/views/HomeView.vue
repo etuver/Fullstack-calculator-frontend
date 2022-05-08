09b68edc15e6ae5fd45043fdff6cd2206d2befd9
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-08 09:50:42 +0200

    added typescript

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue'
-
-// Components
-import HelloWorld from '../components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
-
   components: {
     HelloWorld
   }
