b7eb3c704aea3ef20336dac4e6bd2f191d923ebf
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-11 18:47:45 +0200

    add store data

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,45 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    token: '',
+    email: '',
+    name: ''
   },
   getters: {
+    token (state) {
+      return state.token
+    },
+    isLoggedIn (state) {
+      return state.token !== ''
+    },
+    name (state) {
+      return state.name
+    },
+    email (state) {
+      return state.email
+    }
   },
   mutations: {
+    SET_TOKEN (state, token) {
+      state.token = token
+    },
+    SET_EMAIL (state, email) {
+      state.email = email
+    }
   },
   actions: {
+    setToken (context, { token }) {
+      context.commit('SET_TOKEN', token)
+    },
+    setEmail (context, { email }) {
+      context.commit('SET_TOKEN', email)
+    },
+    setUserInfo (
+      context,
+      { email }
+    ) {
+      context.commit('SET_EMAIL', email)
+    }
   },
   modules: {
   }
