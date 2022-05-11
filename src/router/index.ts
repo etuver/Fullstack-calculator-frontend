0cb9fcdc6d5ff32040c974acaef2b633263e8ae8
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-11 18:48:47 +0200

    Add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,15 +5,26 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () =>
+      import(
+        /* webpackChunkName: "create-user" */ '@/views/HomeView.vue'
+      )
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/register',
+    name: 'register',
+    component: () =>
+      import(
+        /* webpackChunkName: "create-user" */ '@/views/RegisterView.vue'
+      )
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () =>
+      import(
+        /* webpackChunkName: "create-user" */ '@/views/LoginView.vue'
+      )
   }
 ]
 
