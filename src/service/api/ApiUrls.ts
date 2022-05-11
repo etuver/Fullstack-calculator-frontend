f907b267c4a700bfcbfcf0736fb966b0821afce9
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-11 18:46:33 +0200

    add aut url

--- src/service/api/ApiUrls.ts
+++ src/service/api/ApiUrls.ts
@@ -1,4 +1,4 @@
 export const API_URL = 'http://localhost:8888'
-export const AUTH_URL = API_URL + '/auth'
+export const AUTH_URL = API_URL + '/auth/token'
 export const USER_URL = API_URL + '/user'
 export const EXPRESSION_URL = API_URL + '/exp'
