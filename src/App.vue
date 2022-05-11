9b565637bd63e3b7c64e82b440b99afd879886cb
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-11 18:46:54 +0200

    Add content to App

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
   <v-app>
     <v-main>
-      <router-view/>
-      <CalculatorComponent/>
+      <v-col class="ma-5" align="center">
+      <v-btn to="/" class="mr-5">Home</v-btn>
+      <v-btn to="/login" v-if="!store.getters.isLoggedIn"  class="mr-5">Logg inn</v-btn>
+      <v-btn to="/register" v-if="!store.getters.isLoggedIn"  class="mr-5">Registrer</v-btn>
+      </v-col>
+      <v-col align="center" class="ma-5">
+      <v-label > {{ loggedInEmail }} </v-label>
+      </v-col>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
-<script>
+<script setup lang="ts" >
 
-import CalculatorComponent from '@/components/CalculatorComponent'
-export default {
-  name: 'App',
-  components: { CalculatorComponent },
-  data: () => ({
-    //
-  })
+import { onMounted, ref } from 'vue'
+import { useStore } from 'vuex'
+
+const isLoggedin = ref(false)
+const store = useStore()
+const loggedInEmail = ref('')
+
+const updateLoginState = async () => {
+  isLoggedin.value = store.getters.isLoggedIn
+
+  if (isLoggedin.value) {
+    loggedInEmail.value = store.getters.email
+  } else {
+    loggedInEmail.value = ''
+  }
 }
+onMounted(() => {
+  updateLoginState()
+})
+
 </script>
